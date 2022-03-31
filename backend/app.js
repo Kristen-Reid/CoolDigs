cd92134a86b08bce398a97291f86ea403cfb92db
Kristen Reid <kreiddetroit@gmail.com>
2022-03-31 14:15:17 -0400

    backend is setup and working

--- backend/app.js
+++ backend/app.js
@@ -5,5 +5,30 @@ const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
+const routes = require('./routes');
 const { environment } = require('./config');
 const isProduction = environment === 'production';
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.json());
+
+if (!isProduction) app.use(cors());
+
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && 'Lax',
+            httpOnly: true
+        }
+    })
+);
+
+app.use(routes);
+
+module.exports = app;
