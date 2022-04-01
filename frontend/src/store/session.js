d9f7263854f9c2a0d653317fd37a778cc7e3d072
Kristen Reid <kreiddetroit@gmail.com>
2022-04-01 17:11:46 -0400

    setup session thunk, action creators and reducer

---
+++ frontend/src/store/session.js
@@ -0,0 +1,50 @@
+import { csrfFetch } from "./csrf";
+
+const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser';
+
+export const setUser = (user) => {
+    return {
+        type: SET_USER,
+        payload: user
+    }
+}
+
+export const removeUser = () => {
+    return {
+        type: REMOVE_USER
+    }
+}
+
+export const login = (user) => async (dispatch) => {
+    const { credential, password } = user;
+    const res = await csrfFetch('/api/session', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ credential, password })
+    });
+
+    const data = res.json();
+    dispatch(setUser(data.user));
+    return res;
+}
+
+const intialState = { user: null };
+
+const sessionReducer = (state = intialState, action) => {
+    let newState;
+    switch (action.type) {
+        case SET_USER:
+            newState = { ...state };
+            newState.user = action.payload;
+            return newState;
+        case REMOVE_USER:
+            newState = { ...state };
+            newState.user = null;
+            return newState;
+        default:
+            return state;
+    }
+};
+
+export default sessionReducer;
