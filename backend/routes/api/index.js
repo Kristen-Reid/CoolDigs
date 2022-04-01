b82ce180e0d1d67ddd180ced1c3be63d18c55afe
Kristen Reid <kreiddetroit@gmail.com>
2022-03-31 22:53:25 -0400

    finished setting up auth middleware and post route for session.js

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const sessionRouter = require('./session');
+const usersRouter = require('./users');
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
 
 router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
