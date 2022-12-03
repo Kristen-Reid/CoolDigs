e1293d426e4f402a76a37657f32a424700d40cfe
Kristen Reid <kreiddetroit@gmail.com>
2022-12-03 01:10:41 -0500

    dotenv require

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { ValidationError } = require('sequelize');
-require('dotenv').config()
+require('dotenv').config({path: '\\wsl.localhost\Ubuntu\home\kristen\appacademy\mod-5\week-16\cooldigs\backend\.env' })
 
 const routes = require('./routes');
 const { environment } = require('./config');
